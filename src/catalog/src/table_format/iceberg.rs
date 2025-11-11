44308e9382cc8854a3de67585d235328af841f8e
Smith Cruise <chendingchao1@126.com>
2025-11-11 20:28:19 +0800

    clean code
    Signed-off-by: Smith Cruise <chendingchao1@126.com>

--- src/catalog/src/table_format/iceberg.rs
+++ src/catalog/src/table_format/iceberg.rs
@@ -1,16 +1,16 @@
 use crate::table_format::iceberg::metadata_table_provider::IcebergMetadataTableProvider;
-use crate::table_format::iceberg::table_provider::IcebergTableProvider;
 use datafusion::catalog::TableProvider;
 use datafusion::error::DataFusionError;
 use datafusion::sql::TableReference;
+use iceberg::io::FileIO;
+use iceberg::table::StaticTable;
+use iceberg::{NamespaceIdent, TableIdent};
+use iceberg_datafusion::IcebergTableProvider;
 use std::collections::HashMap;
 use std::sync::Arc;
 
-pub mod expr_to_predicate;
-pub mod metadata_table_provider;
-pub mod scan;
-pub mod table_provider;
 mod metadata_scan;
+pub mod metadata_table_provider;
 
 pub struct IcebergTableProviderFactory {}
 
@@ -21,15 +21,47 @@ impl IcebergTableProviderFactory {
         metadata_table_name: Option<&str>,
         properties: HashMap<String, String>,
     ) -> Result<Arc<dyn TableProvider>, DataFusionError> {
-        let iceberg_table_provider =
-            IcebergTableProvider::try_new(table_reference, metadata_location, properties).await?;
-        if let Some(metadata_table_name) = metadata_table_name {
-            let metadata_table_provider = IcebergMetadataTableProvider::try_new(
-                iceberg_table_provider.table,
-                metadata_table_name,
-            )?;
-            return Ok(Arc::new(metadata_table_provider));
+        let schema_name: String;
+        let table_name: String;
+        match table_reference {
+            TableReference::Full {
+                catalog: _,
+                schema,
+                table,
+            } => {
+                schema_name = schema.to_string();
+                table_name = table.to_string();
+            }
+            _ => {
+                return Err(DataFusionError::Plan("invalid table reference".to_string()));
+            }
         }
-        Ok(Arc::new(iceberg_table_provider))
+        let file_io = FileIO::from_path(metadata_location)
+            .map_err(|e| DataFusionError::External(Box::new(e)))?
+            .with_props(properties)
+            .build()
+            .map_err(|e| DataFusionError::External(Box::new(e)))?;
+
+        let iceberg_identifier: TableIdent = TableIdent {
+            namespace: NamespaceIdent::new(schema_name),
+            name: table_name,
+        };
+
+        let iceberg_table =
+            StaticTable::from_metadata_file(metadata_location, iceberg_identifier, file_io)
+                .await
+                .map_err(|e| DataFusionError::External(Box::new(e)))?
+                .into_table();
+
+        if let Some(metadata_table_name) = metadata_table_name {
+            let metadata_table_provider =
+                IcebergMetadataTableProvider::try_new(iceberg_table, metadata_table_name)?;
+            Ok(Arc::new(metadata_table_provider))
+        } else {
+            let iceberg_table = IcebergTableProvider::try_new_from_table(iceberg_table)
+                .await
+                .map_err(|e| DataFusionError::External(Box::new(e)))?;
+            Ok(Arc::new(iceberg_table))
+        }
     }
 }
