ebd80abcd7ad7e2788de370a2660920b2e7f5d95
Smith Cruise <chendingchao1@126.com>
2025-11-11 09:17:10 +0800

    hava conflict
    Signed-off-by: Smith Cruise <chendingchao1@126.com>

--- src/catalog/src/table_format/delta.rs
+++ src/catalog/src/table_format/delta.rs
@@ -1,11 +1,10 @@
 mod table_provider;
 
-use std::collections::HashMap;
-use std::sync::Arc;
+use crate::table_format::delta::table_provider::DeltaTableProvider;
 use datafusion::catalog::TableProvider;
 use datafusion::common::{DataFusionError, TableReference};
-use crate::table_format::iceberg::metadata_table_provider::IcebergMetadataTableProvider;
-use crate::table_format::iceberg::table_provider::IcebergTableProvider;
+use std::collections::HashMap;
+use std::sync::Arc;
 
 pub struct DeltaTableProviderFactory {}
 
@@ -13,18 +12,10 @@ impl DeltaTableProviderFactory {
     pub async fn try_create_table_provider(
         metadata_location: &str,
         table_reference: &TableReference,
-        metadata_table_name: Option<&str>,
         properties: HashMap<String, String>,
     ) -> Result<Arc<dyn TableProvider>, DataFusionError> {
-        let iceberg_table_provider =
-            IcebergTableProvider::try_new(table_reference, metadata_location, properties).await?;
-        if let Some(metadata_table_name) = metadata_table_name {
-            let metadata_table_provider = IcebergMetadataTableProvider::try_new(
-                iceberg_table_provider.table,
-                metadata_table_name,
-            )?;
-            return Ok(Arc::new(metadata_table_provider));
-        }
-        Ok(Arc::new(iceberg_table_provider))
+        let delta_table_provider =
+            DeltaTableProvider::try_new(table_reference, metadata_location, properties).await?;
+        Ok(Arc::new(delta_table_provider))
     }
 }
--- src/catalog/src/table_format/delta/table_provider.rs
+++ src/catalog/src/table_format/delta/table_provider.rs
@@ -1,11 +1,11 @@
 use datafusion::common::TableReference;
 use datafusion::error::DataFusionError;
-use deltalake::{DeltaTable, DeltaTableBuilder, DeltaTableError};
+use deltalake::{DeltaTable, DeltaTableBuilder};
 use std::collections::HashMap;
 use url::Url;
 
+#[derive(Debug)]
 pub struct DeltaTableProvider {
-    pub(crate) delta_table: DeltaTable,
 }
 
 impl DeltaTableProvider {
@@ -13,16 +13,17 @@ impl DeltaTableProvider {
         table_reference: &TableReference,
         table_location: &str,
         properties: HashMap<String, String>,
-    ) -> Result<Self, DataFusionError> {
+    ) -> Result<DeltaTable, DataFusionError> {
         let table_url =
             Url::parse(table_location).map_err(|e| DataFusionError::External(Box::new(e)))?;
         let builder = DeltaTableBuilder::from_uri(table_url)
             .map_err(|e| DataFusionError::External(Box::new(e)))?;
-        Ok(DeltaTableProvider {
-            delta_table: builder
+        let delta_table =
+            builder
                 .load()
                 .await
-                .map_err(|e| DataFusionError::External(Box::new(e)))?,
-        })
+                .map_err(|e| DataFusionError::External(Box::new(e)))?;
+        Ok(delta_table)
     }
 }
+
