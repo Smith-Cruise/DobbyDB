f59feab5f308a5571c3974e88bc55ffd180876a2
Smith Cruise <chendingchao1@126.com>
2026-02-21 23:37:39 +0800

    bump datafusion
    Signed-off-by: Smith Cruise <chendingchao1@126.com>

--- src/catalog/src/table_format/delta.rs
+++ src/catalog/src/table_format/delta.rs
@@ -1,23 +1,36 @@
-mod table_provider;
-
 use crate::storage::StorageCredential;
-use crate::table_format::delta::table_provider::DeltaTableProvider;
 use datafusion::catalog::TableProvider;
-use datafusion::common::Result;
 use datafusion::common::TableReference;
+use datafusion::common::{DataFusionError, Result};
+use deltalake::DeltaTableBuilder;
+use std::collections::HashMap;
 use std::sync::Arc;
+use url::Url;
 
 pub struct DeltaTableProviderFactory {}
 
 impl DeltaTableProviderFactory {
     pub async fn try_create_table_provider(
-        table_reference: &TableReference,
+        _table_reference: &TableReference,
         table_location: &str,
         storage_credential: Option<StorageCredential>,
     ) -> Result<Arc<dyn TableProvider>> {
-        let delta_table_provider =
-            DeltaTableProvider::try_new(table_reference, table_location, storage_credential)
-                .await?;
-        Ok(Arc::new(delta_table_provider))
+        let storage_options = if let Some(storage_credential) = &storage_credential {
+            storage_credential.build_delta_storage_options()
+        } else {
+            HashMap::new()
+        };
+        let table_url =
+            Url::parse(table_location).map_err(|e| DataFusionError::External(Box::new(e)))?;
+        let builder = DeltaTableBuilder::from_url(table_url)
+            .map_err(|e| DataFusionError::External(Box::new(e)))?
+            .with_allow_http(true)
+            .with_storage_options(storage_options);
+        let delta_table = builder
+            .load()
+            .await
+            .map_err(|e| DataFusionError::External(Box::new(e)))?;
+        let delta_scan = delta_table.table_provider().build().await?;
+        Ok(Arc::new(delta_scan))
     }
 }
