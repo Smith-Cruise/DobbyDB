11603c28635847a861ce7d969e00a83335830fc0
Smith Cruise <chendingchao1@126.com>
2026-04-05 22:46:16 +0800

    Add Hive table support to Glue catalog

--- src/catalog/src/table_format/hive/hive_partition.rs
+++ src/catalog/src/table_format/hive/hive_partition.rs
@@ -1,6 +1,7 @@
+use aws_sdk_glue::types::Partition as GluePartition;
 use datafusion::common::Result;
 use datafusion::error::DataFusionError;
-use hive_metastore::Partition;
+use hive_metastore::Partition as HMSPartition;
 
 #[derive(Debug)]
 pub struct HivePartition {
@@ -9,31 +10,34 @@ pub struct HivePartition {
 }
 
 impl HivePartition {
-    pub fn try_new_from_hms_partition(partition: &Partition) -> Result<Self> {
-        let sd = match &partition.sd {
-            Some(sd) => sd,
-            None => {
-                return Err(DataFusionError::Internal(String::from(
-                    "partition sd not existed",
-                )));
-            }
-        };
-        let location = match &sd.location {
-            Some(location) => location.to_string(),
-            None => {
-                return Err(DataFusionError::Internal(String::from(
-                    "partition sd location not existed",
-                )));
-            }
-        };
-        let partition_values = match &partition.values {
-            Some(partition) => partition.iter().map(|v| v.to_string()).collect(),
-            None => {
-                return Err(DataFusionError::Internal(String::from(
-                    "partition values existed",
-                )));
-            }
-        };
+    pub fn try_new_from_hms_partition(partition: &HMSPartition) -> Result<Self> {
+        let sd = partition
+            .sd
+            .as_ref()
+            .ok_or_else(|| DataFusionError::Internal("partition sd not existed".to_string()))?;
+        Self::try_new(
+            sd.location.as_deref(),
+            partition
+                .values
+                .as_ref()
+                .map(|partition| partition.iter().map(|v| v.to_string()).collect()),
+        )
+    }
+
+    pub fn try_new_from_glue_partition(partition: &GluePartition) -> Result<Self> {
+        let sd = partition
+            .storage_descriptor()
+            .ok_or_else(|| DataFusionError::Internal("partition sd not existed".to_string()))?;
+        Self::try_new(sd.location(), partition.values.clone())
+    }
+
+    fn try_new(location: Option<&str>, partition_values: Option<Vec<String>>) -> Result<Self> {
+        let location = location.map(ToString::to_string).ok_or_else(|| {
+            DataFusionError::Internal("partition sd location not existed".to_string())
+        })?;
+        let partition_values = partition_values
+            .ok_or_else(|| DataFusionError::Internal("partition values not existed".to_string()))?;
+
         Ok(HivePartition {
             location,
             partition_values,
